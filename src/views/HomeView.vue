0f981eb0a835155e84b419d56cfe978718899330
ChanMyaeAung <chanmyae.cma30@gmail.com>
2022-11-10 14:47:08 +0630

    getPosts composable function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,19 @@
 <template>
+<div v-if="error">
+  {{error}}
+</div>
+<div v-if="posts.length>0">
   <PostsLitst :posts="posts"></PostsLitst>
+</div>
+<div v-else>
+  loading...
+</div>
 </template>
 
 <script>
 import PostsLitst from '../components/PostsLitst'
 import { computed, ref } from '@vue/reactivity'
+import getPosts from '../composables/getPosts'
 
 
 
@@ -12,10 +21,9 @@ export default {
   components: { PostsLitst },
  
   setup(){
-    let posts = ref([
-     
-    ])
-    return {posts}
+    let {posts,error,load} = getPosts()
+    load();
+    return {posts,error}
   }
 }
 </script>

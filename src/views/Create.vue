7ae99e6b05cee70562870933ac3fc7f2c6e934ae
ChanMyaeAung <chanmyae.cma30@gmail.com>
2022-11-25 12:41:31 +0630

    create post and save into our firestore

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -19,6 +19,7 @@
 <script>
 import { ref } from '@vue/reactivity'
 import {useRouter} from 'vue-router'
+import {db} from '../firebase/config'
 
 export default {
      setup(){
@@ -35,19 +36,13 @@ export default {
         }
         
         let addPost=async()=>{
-            await fetch("http://localhost:3000/posts",{
-                method:"POST",
-                headers:{
-                    "Content-type" : "application/json"
-                },
-                body:JSON.stringify(
-                {
+          let newPost =  {
                     title : title.value,
                     body : body.value,
                     tags : tags.value
                 }
-                )
-            })
+            let res = await db.collection("posts").add(newPost)
+            
             router.push("/")
         }
 
